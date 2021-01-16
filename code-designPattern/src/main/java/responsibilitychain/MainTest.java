aa8cac2a536a570e95f937c6f779559aa9b80515
hesong <l>
2021-01-16 15:30:19 +0800

    添加备注

--- code-designPattern/src/main/java/responsibilitychain/MainTest.java
+++ code-designPattern/src/main/java/responsibilitychain/MainTest.java
@@ -1,8 +1,13 @@
 package responsibilitychain;
 
 public class MainTest {
+    /**
+     * 职责链模式
+     * 模拟不同的账单有不同级别的人员处理
+     * @param args
+     */
     public static void main(String[] args) {
-        PurchaseRequest request = new PurchaseRequest(1, 23, 666f);
+        PurchaseRequest request = new PurchaseRequest(1, 23, 6666f);
 
         DepartmentApprover departmentApprover = new DepartmentApprover("张主任");
         ViceSchoolMaster viceSchoolMaster = new ViceSchoolMaster("陈副校长");
@@ -13,7 +18,7 @@ public class MainTest {
         schoolMaster.setApprover(departmentApprover);
 
         departmentApprover.processRequest(request);
-
+        request = new PurchaseRequest(1,22, 44533);
         schoolMaster.processRequest(request);
     }
 }
